7b34044a07322bcc58053cab89aff1b5bd279dd6
George <george.dma@gmail.com>
2018-05-15 16:56:57 +0300

    feat: setup tooling and start page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const hello = (
+  <div className="App">
+    <h1>Hello guys</h1>
+  </div>
+);
+
+class App extends Component {
+  render() {
+    return hello;
+  }
+}
+
+export default App;
