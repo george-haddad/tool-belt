3b2cfce70f262db0e7b3acd8ac698623d8085ff1
George <george.dma@gmail.com>
2018-05-21 16:26:34 +0300

    feat: add error and success response handling

--- src/components/axfr/index.jsx
+++ src/components/axfr/index.jsx
@@ -5,6 +5,12 @@ import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import superagent from 'superagent';
 
 const styles = theme => ({
   container: {
@@ -28,6 +34,15 @@ const styles = theme => ({
   margin: {
     margin: theme.spacing.unit,
   },
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    color: theme.palette.text.primary,
+  },
+  icon: {
+    margin: theme.spacing.unit,
+  },
 });
 
 type Props = {
@@ -35,43 +50,124 @@ type Props = {
 };
 
 type State = {
-  domain: string,
+  axfr: {
+    onlinecheck: {
+      domain: string,
+      affectedDns: ?Array<string>,
+      error: boolean,
+    },
+  },
 };
 
 class AxfrPanel extends Component<Props, State> {
   constructor(props) {
     super(props);
     this.state = {
-      domain: '',
+      axfr: {
+        onlinecheck: {
+          domain: '',
+          affectedDns: undefined,
+          error: false,
+        },
+      },
     };
   }
 
   handleDomainChange = (event: any) => {
     this.setState({
-      domain: event.target.value,
+      axfr: {
+        onlinecheck: {
+          domain: event.target.value,
+          affectedDns: this.state.axfr.onlinecheck.affectedDns,
+          error: this.state.axfr.onlinecheck.error,
+        },
+      },
     });
+  };
+
+  axfrOnlineCheck = () => {
+    const { domain } = this.state.axfr.onlinecheck;
+    superagent
+      .get(`http://api.axfrcheck.com/api/check/axfr/${domain}`)
+      .set('Accept', 'application/json')
+      .set('Access-Control-Allow-Origin', '*')
+      .then(res => {
+        if (res.body.data[0].affected_dns) {
+          const affectedDns = res.body.data[0].affected_dns;
+          this.setState({
+            axfr: {
+              onlinecheck: {
+                domain: this.state.axfr.onlinecheck.domain,
+                affectedDns,
+                error: this.state.axfr.onlinecheck.error,
+              },
+            },
+          });
+        }
+      })
+      .catch(() => {
+        this.setState({
+          axfr: {
+            onlinecheck: {
+              domain: this.state.axfr.onlinecheck.domain,
+              affectedDns: this.state.axfr.onlinecheck.affectedDns,
+              error: true,
+            },
+          },
+        });
+      });
   };
 
   render() {
     const { classes } = this.props;
-    const { domain } = this.state;
+    const { axfr } = this.state;
+
+    const SuccessLogo = () => {
+      if (axfr.onlinecheck.affectedDns && axfr.onlinecheck.domain.length > 0) {
+        if (axfr.onlinecheck.affectedDns.length > 0) {
+          return <ErrorIcon className={classes.icon} color="error" />;
+        }
+
+        return <CheckIcon className={classes.icon} color="primary" />;
+      }
+      return <div />;
+    };
 
     return (
       <div>
-        <FormControl className={classes.margin}>
-          <TextField
-            id="domain"
-            label="Domain"
-            className={classes.textField}
-            value={domain}
-            onChange={event => this.handleDomainChange(event)}
-            margin="normal"
-          />
-        </FormControl>
-        <br />
-        <Button variant="outlined" className={classes.button} color="primary">
-          Go
-        </Button>
+        <div>
+          <FormControl className={classes.margin}>
+            <TextField
+              id="domain"
+              label="Domain"
+              error={axfr.onlinecheck.error}
+              className={classes.textField}
+              value={axfr.onlinecheck.domain}
+              onChange={event => this.handleDomainChange(event)}
+              margin="normal"
+            />
+          </FormControl>
+          <Button
+            variant="outlined"
+            className={classes.button}
+            color="primary"
+            size="small"
+            onClick={this.axfrOnlineCheck}
+          >
+            Go
+          </Button>
+          <SuccessLogo />
+        </div>
+        <div className={classes.root}>
+          <List component="AffectedDnsList">
+            {axfr.onlinecheck.affectedDns &&
+              axfr.onlinecheck.affectedDns.map(domainName => (
+                <ListItem button key={domainName}>
+                  <ListItemText primary={domainName} />
+                </ListItem>
+              ))}
+          </List>
+        </div>
       </div>
     );
   }
